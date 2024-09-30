4a8a429f1b5382ad301bc40049e908a71470ff9a
Abdallah Al Sawae´er <ajarmah@Abdallahs-MacBook-Air.local>
2024-09-30 20:00:34 +0200

    added functionalty to generate final HTML file

--- main.py
+++ main.py
@@ -32,7 +32,15 @@ def main():
     new_url = f'{URL}{name}'
     res = requests.get(new_url, headers={'X-Api-Key': 'UkkpAvnGuZ8I1p6h4hTfkA==b9sZdFDa2MqJyuyC'}).json()
     animals_html = animals_details(res)
-    print(animals_html)
+
+    with open('animals_template.html', 'r') as f:
+        html_data = f.read()
+
+    final_html_data = html_data.replace('__REPLACE_ANIMALS_INFO__', animals_html)
+
+    with open('animals.html', 'w') as f:
+        f.write(final_html_data)
+    print('Website was successfully generated to the file animals.html.')
 
 
 if __name__ == "__main__":
